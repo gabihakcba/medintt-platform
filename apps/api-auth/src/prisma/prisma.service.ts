3a83131d94f80f32c262c572934c6539e8b51cd0
Gabriel Hak <gabrielhakcba@gmail.com>
2025-12-11 01:20:08 -0300

    database-auth check

--- apps/api-auth/src/prisma/prisma.service.ts
+++ apps/api-auth/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@medintt/database';
+import { PrismaClient } from '@medintt/database-auth';
 
 @Injectable()
 export class PrismaService
--- packages/database/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client/client';
-
-export { PrismaClient };
-
-export * from '@prisma/client';

cf85e840cb0fa5b5017e181f32f92d52a24297a6
GrupoMedintt <medintt@srv1028207.hstgr.cloud>
2026-02-24 11:35:32 +0000

    nuevo esquema

--- packages/databases/database-medintt4/prisma/views/dbo/rpt_Incidentes_de_Empresas.sql
+++ packages/databases/database-medintt4/prisma/views/dbo/rpt_Incidentes_de_Empresas.sql
@@ -1,22 +1,4 @@
 SELECT
-  p.Id,
-  p.Fecha_Practica AS Fecha,
-  'PRACTICA' AS Clase,
-  ISNULL(pa.Apellido, '') + ', ' + ISNULL(pa.Nombre, '') AS Paciente,
-  ISNULL(pa.NroDocumento, '') AS DNI,
-  ISNULL(pr.Apellido, '') + ', ' + ISNULL(pr.Nombre, '') AS Profesional,
-  p.InformeProfesional AS Notas,
-  af.Id_Prestataria
-FROM
-  dbo.Practicas AS p
-  LEFT JOIN dbo.Profesionales AS pr ON p.Id_Profesional_Informante = pr.Id
-  LEFT JOIN dbo.Pacientes AS pa ON p.Id_Paciente = pa.Id
-  LEFT JOIN dbo.Afiliacion_Pacientes AS af ON af.Id = p.Id_Afiliacion
-WHERE
-  p.Status = N'INFORMADA'
-UNION
-ALL
-SELECT
   h.id AS Id,
   h.Fecha,
   'CONSULTA AMBULATORIA' AS Clase,
@@ -31,4 +13,4 @@ FROM
   JOIN dbo.Profesionales AS pr ON h.Id_Profesional = pr.Id
   LEFT JOIN dbo.Pacientes AS pa ON h.Id_Paciente = pa.Id
 WHERE
-  h.Tipo = N'LABORAL';
+  (h.Tipo = N'LABORAL');
